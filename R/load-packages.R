94809d74b837b278c9fbc6095c8948e39badfb07
kfsgiorgos <georgios.kaiafas@uni.lu>
2019-06-07 17:08:38 +0200

    add Rscripts I have lost

--- R/load-packages.R
+++ R/load-packages.R
@@ -1,6 +1,6 @@
 # install & load the required packages
 pkg <- c("ggplot2", "lubridate", "data.table", "purrr", "reticulate", "foreign",
-        "pROC", "caret", "dplyr", "esquisse", "stringr", "readtext")
+        "pROC", "caret", "dplyr", "esquisse", "stringr", "readtext", "esquisse")
 new.pkg <- pkg[!(pkg %in% installed.packages())]
 if (length(new.pkg)) {
   install.packages(new.pkg, dependencies = TRUE)
