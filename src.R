dce8094c3511bf6b1bd95fe1655c5bfcb7a07e4b
kfsgiorgos <georgios.kaiafas@uni.lu>
2019-06-20 15:32:43 +0200

    src script. start everyday with this script

--- src.R
+++ src.R
@@ -1,15 +1,10 @@
 # selected_configuration <- "macBook"
 
 if(selected_configuration == "macBook"){
-  print("1")
   config <- config::get(file = "~/R Language Default Dir/Github-projects/multiview-oneclass/config.yml")
-  print("2")
   setwd(config::get("directory"))
-  print("3")
   source(config::get("source_scripts"))
-  print("4")
   use_condaenv(config::get("conda_env"))
-  print("5")
   }
 
 
