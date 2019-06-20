49aa0f6bd21bc295886bf0c466110418c87c0235
kfsgiorgos <georgios.kaiafas@uni.lu>
2019-06-20 15:12:11 +0200

    commit the latest version of this file

--- R/non-run.R
+++ R/non-run.R
@@ -1,7 +1,5 @@
 library(usethis)
-#install.packages("readtext")
 
-library(readtext)
 # Create a new package -------------------------------------------------
 usethis::use_r("load-packages")
 usethis::use_r("construct-tabular-outlier")
@@ -10,3 +8,56 @@ usethis::use_r("cross-training")
 usethis::use_r("OCSVM-unsupervised-representation-functions")
 usethis::use_r("evaluate-unsupervised-representation")
 usethis::use_r("commandline-script")
+usethis::use_r("pipeline-newdataset")
+usethis::use_r("configuration")
+
+
+
+
+
+# setwd("~/R Language Default Dir/Github-projects/multiview-oneclass/")
+# source("R/load-packages.R")
+# use_condaenv("r-reticulate")
+# reticulate::source_python("Python/sklearn-outlier-algos.py")
+# 
+# 
+# # load packages & source scripts ------------------------------------------
+# setwd("~/GitHub_projects/multiview-oneclass/")
+# source("R/Functional/OCSVM-multiview.R")
+# source("R/Functional/construct-tabular-outlier.R")
+# 
+# 
+# 
+# 
+# # load packages & source scripts ------------------------------------------
+# setwd("~/R Language Default Dir/Github-projects/multiview-oneclass/")
+# source("R/load-packages.R")
+# #use_condaenv("r-reticulate")
+# reticulate::source_python("Python/sklearn-outlier-algos.py")
+# 
+# 
+# 
+# setwd("~/R Language Default Dir/Github-projects/multiview-oneclass/")
+# source("R/load-packages.R")
+# use_condaenv("r-reticulate")
+# reticulate::source_python("Python/sklearn-outlier-algos.py")
+# 
+# 
+# 
+# 
+# # load packages & source scripts ------------------------------------------
+# setwd("~/R Language Default Dir/Github-projects/multiview-oneclass/")
+# source("R/load-packages.R")
+# use_condaenv("r-reticulate")
+# reticulate::source_python("Python/sklearn-outlier-algos.py")
+# 
+# 
+# setwd("~/R Language Default Dir/Github-projects/multiview-oneclass/")
+# source("R/load-packages.R")
+# use_condaenv("r-reticulate")
+# reticulate::source_python("Python/sklearn-outlier-algos.py")
+# 
+# 
+# 
+# 
+# # load packages & source scripts ------------------------------------------
