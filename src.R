45a69fb4d4e671e3cf8cbb48d76d2601639a6ef7
kfsgiorgos <georgios.kaiafas@uni.lu>
2019-06-28 14:31:55 +0100

    enhance src script

--- src.R
+++ src.R
@@ -1,11 +1,31 @@
 # selected_configuration <- "macBook"
+# selected_configuration <- "Sherlock"
 
 if(selected_configuration == "macBook"){
-  config <- config::get(file = "~/R Language Default Dir/Github-projects/multiview-oneclass/config.yml")
-  setwd(config::get("directory"))
-  source(config::get("source_scripts"))
-  use_condaenv(config::get("conda_env"))
+  config_file_path <- "~/R Language Default Dir/Github-projects/multiview-oneclass/config.yml"
+  setwd(config::get("directory", 
+                    file = config_file_path, 
+                    config = "default"))
+  source(config::get("source_scripts", 
+                     file = config_file_path, 
+                     config = "default"))
+  use_condaenv(config::get("conda_env", config = "default"))
+  loaded_config_type <- "default"
   }
+
+
+if(selected_configuration == "sherlock"){
+  config_file_path <- "~/GitHub_projects/multiview-oneclass/config.yml"
+  setwd(config::get("directory", 
+                    file = config_file_path,
+                    config = "production"))
+  source(config::get("source_scripts", 
+                     file = config_file_path,
+                     config = "production"))
+  loaded_config_type <- "production"
+}
+
+
 
 
 
