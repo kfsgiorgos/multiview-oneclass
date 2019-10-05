005482dca32564decc833b451c0c977b75227713
kfsgiorgos <georgios.kaiafas@uni.lu>
2019-10-06 01:56:27 +0200

    add one package

--- R/load-packages.R
+++ R/load-packages.R
@@ -2,7 +2,7 @@
 pkg <- c("ggplot2", "lubridate", "data.table", "purrr", "reticulate", "foreign",
         "pROC", "caret", "dplyr", "esquisse", "stringr", "readtext", "esquisse",
         "dbscan", "philentropy", "glue", "readtext", "config", "doParallel",
-        "foreach", "devtools", "scmamp", "hmeasure")
+        "foreach", "devtools", "scmamp", "hmeasure", "rlist")
 new.pkg <- pkg[!(pkg %in% installed.packages())]
 if (length(new.pkg)) {
   install.packages(new.pkg, dependencies = TRUE)
