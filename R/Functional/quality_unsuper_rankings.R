d127a0c4fbc461c17cf450bf126f707d13694ba4
Georgios Kaiafas <kfsgiorgos@gmail.com>
2020-02-27 11:00:20 +0100

    minor fix

--- R/Functional/quality_unsuper_rankings.R
+++ R/Functional/quality_unsuper_rankings.R
@@ -392,22 +392,19 @@ get_results_DT_average <- function(given_datasetname_1, version_string, col_name
     version_string1 <- ""
     list_names <- as.list(paste0(given_datasetname_1, "_withoutdupl_norm"))
     list_names_short <- as.list(given_datasetname_1)
-  }  else{
+  } else if(version_string == "catremoved"){
+    list_names <- as.list(paste0(given_datasetname_1, "_withoutdupl_norm_catremoved"))
+    list_names_short <- as.list(given_datasetname_1)
+  } else if (version_string == "catremoved1"){
+    list_names <- as.list(paste0(given_datasetname_1, "_withoutdupl_catremoved"))
+    list_names_short <- as.list(given_datasetname_1)
+  } else{
     version_string1 <-  paste0(version_string, "_")
     list_names <- as.list(c(paste0(given_datasetname_1, "_withoutdupl_norm_", version_string1, "v0", 1:9),
                             paste0(given_datasetname_1, "_withoutdupl_norm_", version_string1, "v10")))
     list_names_short <- as.list(c(paste0(given_datasetname_1, "_v0", 1:9), 
                                   paste0(given_datasetname_1, "_v10" )))
     }
-  if (version_string == "catremoved"){
-    list_names <- as.list(paste0(given_datasetname_1, "_withoutdupl_norm_catremoved"))
-    list_names_short <- as.list(given_datasetname_1)
-  }
-  if (version_string == "catremoved1"){
-    list_names <- as.list(paste0(given_datasetname_1, "_withoutdupl_catremoved"))
-    list_names_short <- as.list(given_datasetname_1)
-  }
-  
 
   res_auc <- rbindlist(pmap(list(list_names, list_names_short), get_res_dataset))
   res_auc[, col:= col_name]
